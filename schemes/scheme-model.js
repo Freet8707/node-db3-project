f68059a4dd9219b3f7e8e6f8313c799c98c5a4a5
Freet8707 <freet8707@gmail.com>
2020-09-24 01:03:09 -0500

    findSteps function added in helper file

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -17,7 +17,19 @@ const findById = async (schemeID) => {
     return scheme[0];
 }
 
+const findSteps = async (schemeID) => {
+
+    const steps = await db('steps as s')
+        .innerJoin('schemes as sc', 'sc.id', 's.scheme_id')
+        .select('sc.scheme_name as scheme name', 's.step_number as step number', 's.instructions')
+        .where('s.scheme_id', schemeID)
+        .orderBy('s.step_number');
+
+    return steps
+}
+
 module.exports = {
     find,
-    findById
+    findById,
+    findSteps
 }
